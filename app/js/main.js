1761be65d8e9feecf4fe15a8d3d1f1a58b4fc70b
DEN <voland2114@gmail.com>
2019-11-27 23:25:31 +0700

    add product-slider & product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-$(()=>{
+$(function(){
    var mixer = mixitup('.products__inner-box');
+
+   $(".rate-star").rateYo({
+      rating: 3.6,
+      starWidth: "12px",
+      readOnly: true,
+    });
+
+    $('.product-slider__inner').slick({
+      // infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots:true,
+      arrows:false,
+    });
 
 })

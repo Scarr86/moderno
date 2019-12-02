190b2fe5bfd64be1442beba71c637de507c942e0
DEN <voland2114@gmail.com>
2019-12-02 22:50:23 +0700

    do new item page

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,35 @@
-$(function(){
-   var mixer = mixitup('.products__inner-box');
+$(function () {
 
+   try {
+      var mixer = mixitup('.products__inner-box');
+   } catch (error) {
+      console.log(error);
+   }
    $(".rate-star").rateYo({
       rating: 3.6,
       starWidth: "12px",
       readOnly: true,
-    });
+   });
 
-    $('.product-slider__inner').slick({
+
+   $('.product-slider__inner').slick({
       // infinite: true,
       slidesToShow: 4,
       slidesToScroll: 4,
-      dots:true,
-      arrows:false,
-    });
+      dots: true,
+      arrows: false,
+   });
+
+   $(".js-range-slider").ionRangeSlider({
+      type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$",
+        skin: "round",
+        
+        
+  });
 
 })

3d9b47a96848369a3c3dd1ad76c53cfff4660c8e
DEN <voland2114@gmail.com>
2019-12-06 16:11:12 +0700

    add popup login

--- app/js/main.js
+++ app/js/main.js
@@ -41,20 +41,24 @@ $(function () {
    //=========
 
    // login
+
+
+
+   // my experiment
    // $('.header__btn-logout').removeClass('header__btn-login--on');
    // $('.header__btn-login').removeClass('header__btn-login--off');
    // $('.header__btn-register').removeClass('header__btn-login--off');
 
-   $('.header__btn-login').on('click', function () {
-      $(this).addClass('header__btn-login--off');
-      $('.header__btn-logout').addClass('header__btn-login--on');
-      $('.header__btn-register').addClass('header__btn-login--off');
-   })
-   $('.header__btn-logout').on('click', function () {
-      $(this).removeClass('header__btn-login--on');
-      $('.header__btn-login').removeClass('header__btn-login--off');
-      $('.header__btn-register').removeClass('header__btn-login--off');
-   })
+   // $('.header__btn-login').on('click', function () {
+   //    $(this).addClass('header__btn-login--off');
+   //    $('.header__btn-logout').addClass('header__btn-login--on');
+   //    $('.header__btn-register').addClass('header__btn-login--off');
+   // })
+   // $('.header__btn-logout').on('click', function () {
+   //    $(this).removeClass('header__btn-login--on');
+   //    $('.header__btn-login').removeClass('header__btn-login--off');
+   //    $('.header__btn-register').removeClass('header__btn-login--off');
+   // })
    //=============
 
 
@@ -69,6 +73,17 @@ $(function () {
    });
 
    //============
+
+   // tabs
+   $('.product-one__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+         $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+         $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+         $(this).addClass('active');
+         $('#'+id).addClass('active-tab').fadeIn();
+         return false;
+      });
+   //=============
 
 
    try {

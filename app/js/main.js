a7d95086ec7f42a7d0cf7c87189380cdc66448c6
DEN <voland2114@gmail.com>
2019-12-07 09:29:22 +0700

    add contact & settings page, fix page, feat js

--- app/js/main.js
+++ app/js/main.js
@@ -54,11 +54,11 @@ $(function () {
    //    $('.header__btn-logout').addClass('header__btn-login--on');
    //    $('.header__btn-register').addClass('header__btn-login--off');
    // })
-   // $('.header__btn-logout').on('click', function () {
-   //    $(this).removeClass('header__btn-login--on');
-   //    $('.header__btn-login').removeClass('header__btn-login--off');
-   //    $('.header__btn-register').removeClass('header__btn-login--off');
-   // })
+   $('.header__btn-logout').on('click', function () {
+      $(this).removeClass('header__btn-login--on');
+      $('.header__btn-login').removeClass('header__btn-login--off');
+      $('.header__btn-register').removeClass('header__btn-login--off');
+   })
    //=============
 
 
@@ -75,10 +75,10 @@ $(function () {
    //============
 
    // tabs
-   $('.product-one__tabs .tab').on('click', function(event) {
+   $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
       var id = $(this).attr('data-id');
-         $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-         $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+         $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+         $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
          $(this).addClass('active');
          $('#'+id).addClass('active-tab').fadeIn();
          return false;
